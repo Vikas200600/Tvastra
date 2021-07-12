647ea0798f13cb341f273a93519dd849657e00b1
Vikas M <vikas200600@gmail.com>
2021-07-12 17:08:32 +0530

    All Pages Complete

---
+++ src/client/assets/scripts/first.js
@@ -0,0 +1,35 @@
+var questions = document.querySelectorAll('.question');
+var answers = document.querySelectorAll(".answer");
+
+
+questions.forEach(question => {
+    question.addEventListener("click", () => {
+        question.classList.toggle("active");
+        var answer = question.nextElementSibling;
+        if(answer.style.display == "block"){
+            answer.style.display = "none";
+        } else {
+            answer.style.display = "block";
+        }
+        // answers.forEach(ans => {
+        //     ans.style.display = "none";
+        // })
+        // answer.style.display="block";
+    })    
+});
+
+
+var slides = document.querySelectorAll(".test-cards");
+var sliders = document.querySelectorAll(".slider");
+var showSlide = (n) => {
+    slides[n].style.display = "grid";
+    sliders[n].style.backgroundColor = "#0173b2"
+    if(n == 0) {
+        slides[1].style.display = "none";
+    sliders[1].style.backgroundColor = "#9cbfda"
+    }else {
+    sliders[0].style.backgroundColor = "#9cbfda"
+    slides[0].style.display ="none";
+    }
+
+}
