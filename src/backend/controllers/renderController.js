dd4e5f93540e21b32c68818efc1e2cc125c8feda
Vikas M <vikas200600@gmail.com>
2021-07-31 16:07:06 +0530

    Appointment View For User Complete

--- src/backend/controllers/renderController.js
+++ src/backend/controllers/renderController.js
@@ -1,105 +1,113 @@
 const Doctor = require('./../models/DoctorModel');
 
 let renderLogin = (req, res) => {
-    res.render('login',{
-        flash : req.flash(),
-        loggedIn : null,
-        username : null
+    res.render('login', {
+        flash: req.flash(),
+        loggedIn: null,
+        username: null
     });
 }
 
 let renderMobileLogin = (req, res) => {
-    res.render('mobileLogin',{
-        flash : req.flash(),
-        loggedIn : null,
-        username : null
+    res.render('mobileLogin', {
+        flash: req.flash(),
+        loggedIn: null,
+        username: null
     });
 }
 
 let renderSignUp = (req, res) => {
-    res.render('signUp',{
-        flash : req.flash(),
-        loggedIn : null,
-        username : null
+    res.render('signUp', {
+        flash: req.flash(),
+        loggedIn: null,
+        username: null
     });
 }
 
 let renderDetails = (req, res) => {
-    res.render('docProfile',{
-        flash : req.flash(),
-        loggedIn : null
+    res.render('docProfile', {
+        flash: req.flash(),
+        loggedIn: null
     });
 }
 
-let renderHome = (req, res) =>{
-    res.render('index',{
-        flash : req.flash(),
-        loggedIn : true,
-        username : req.session.name
+let renderHome = (req, res) => {
+    res.render('index', {
+        flash: req.flash(),
+        loggedIn: true,
+        username: req.session.name
     });
 }
 
 
 let renderProfile = (req, res) => {
-    res.render('profile',{
-        loggedIn : "true",
-        username : req.session.name
+    res.render('profile', {
+        loggedIn: "true",
+        username: req.session.name
+    });
+}
+
+let renderAppointment = (req, res) => {
+    res.render('appointments', {
+        loggedIn: "true",
+        username: req.session.name
     });
 }
 
 let renderDoctor = async (req, res) => {
     let allDoctors = await Doctor.find();
-    res.render('doctor',{
-        loggedIn : true,
-        username : req.session.name,
-        allDoctors : allDoctors
+    res.render('doctor', {
+        loggedIn: true,
+        username: req.session.name,
+        allDoctors: allDoctors
     });
 }
 
 let renderHospital = (req, res) => {
-    res.render('hospital',{
-        loggedIn : true,
-        username : req.session.name
+    res.render('hospital', {
+        loggedIn: true,
+        username: req.session.name
     });
 }
 
 let renderTreatment = (req, res) => {
-    res.render('aboutTreatment',{
-        loggedIn : true,
-        username : req.session.name
+    res.render('aboutTreatment', {
+        loggedIn: true,
+        username: req.session.name
     });
 }
 
 let renderPlus = (req, res) => {
-    res.render('tvastraPlus',{
-        loggedIn : true,
-        username : req.session.name
+    res.render('tvastraPlus', {
+        loggedIn: true,
+        username: req.session.name
     });
 }
 
 
 let renderAbout = (req, res) => {
-    req.session.name ? res.render('aboutUs',{ loggedIn : true, username : req.session.name}) : res.render('aboutUs', { loggedIn :false, username : null});
+    req.session.name ? res.render('aboutUs', { loggedIn: true, username: req.session.name }) : res.render('aboutUs', { loggedIn: false, username: null });
 }
 
 let renderFaq = (req, res) => {
-    req.session.name ? res.render('faq',{ loggedIn : true, username : req.session.name}) : res.render('faq', { loggedIn : false, username : null });
+    req.session.name ? res.render('faq', { loggedIn: true, username: req.session.name }) : res.render('faq', { loggedIn: false, username: null });
 }
 
 
 
 
 module.exports = {
-    renderLogin : renderLogin,
-    renderMobileLogin : renderMobileLogin,
-    renderSignUp : renderSignUp,
-    renderDetails : renderDetails, 
-    renderHome : renderHome,
-    renderProfile :renderProfile,
-    renderDoctor : renderDoctor,
-    renderHospital : renderHospital,
-    renderTreatment : renderTreatment,
-    renderPlus : renderPlus,
-    renderAbout : renderAbout,
-    renderFaq : renderFaq
+    renderLogin: renderLogin,
+    renderMobileLogin: renderMobileLogin,
+    renderSignUp: renderSignUp,
+    renderDetails: renderDetails,
+    renderHome: renderHome,
+    renderProfile: renderProfile,
+    renderAppointment: renderAppointment,
+    renderDoctor: renderDoctor,
+    renderHospital: renderHospital,
+    renderTreatment: renderTreatment,
+    renderPlus: renderPlus,
+    renderAbout: renderAbout,
+    renderFaq: renderFaq
 }
--- src/backend/routes/routes.js
+++ src/backend/routes/routes.js
@@ -15,37 +15,25 @@ router.route('/temp').get(testController.tempRoute);
 router.route('/adduser').get(testController.addUser);
 router.route('/users').get(testController.getUsers);
 
-
-
 router.route('/login').get(renderController.renderLogin).post(authController.login);
 router.route('/signup').get(renderController.renderSignUp).post(authController.signup);
 router.route('/details').get(renderController.renderDetails);
 router.route('/submit_details').post(authController.submitDetails);
 router.route('/mobilelogin').get(renderController.renderMobileLogin);
-router.route('/profile').get(renderController.renderProfile);
 
-
+//dashboard routes
+router.route('/profile').get(sessionAuth.redirectlogin, renderController.renderProfile);
+router.route('/dashboard_appointments').get(sessionAuth.redirectlogin, renderController.renderAppointment);
 
 router.route('/requestotp').post(otpController.otpRequest);
 router.route('/verify').post(otpController.otpVerify);
 
-router.route('/').get(sessionAuth.redirectlogin ,renderController.renderHome);
-router.route('/doctors').get(sessionAuth.redirectlogin ,renderController.renderDoctor);
-router.route('/hospitals').get(sessionAuth.redirectlogin ,renderController.renderHospital);
-router.route('/treatments').get(sessionAuth.redirectlogin ,renderController.renderTreatment);
+router.route('/').get(sessionAuth.redirectlogin, renderController.renderHome);
+router.route('/doctors').get(sessionAuth.redirectlogin, renderController.renderDoctor);
+router.route('/hospitals').get(sessionAuth.redirectlogin, renderController.renderHospital);
+router.route('/treatments').get(sessionAuth.redirectlogin, renderController.renderTreatment);
 router.route('/about').get(renderController.renderAbout);
 router.route('/faq').get(renderController.renderFaq);
 router.route('/Plus').get(renderController.renderPlus);
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = router;
