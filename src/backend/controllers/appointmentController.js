2307b7a8444c8e05fcbcbad7b451c3c46af2ae18
Vikas M <vikas200600@gmail.com>
2021-08-15 23:55:07 +0530

    book appointment starters complete

--- src/backend/controllers/appointmentController.js
+++ src/backend/controllers/appointmentController.js
@@ -1,10 +1,27 @@
+const mongoose = require('mongoose');
+
 const Appointment = require('./../models/appointmentModel');
 const Slot = require('./../models/SlotModel');
 
 let bookAppointment = async (req, res) => {
     console.log(req.query);
     console.log(req.params);
-    res.redirect('/');
+    let newAppointment = await Appointment.create({
+        slotId: req.query.slotId,
+        userId: req.query.userId,
+        doctorId: req.params.doctorId,
+        appointmentDate: req.date
+    });
+    if(newAppointment) {
+        await Slot.findOneAndUpdate(
+            {"subSlots._id": mongoose.Types.ObjectId(req.query.slotId)},
+            { $set: { 'subSlots.$.isBooked': true }}
+        );
+        res.redirect('/confirm-booking');
+    } else {
+        res.redirect('/doctors')
+    }
+    // let selectedSlot = await Slot.find({})
 }
 
 module.exports = {
